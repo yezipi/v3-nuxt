623d770751302eafa9dfaa348470ce0287e69e9a
zzc <974683998@qq.com>
2022-06-08 22:01:23 +0800

    feat: 页脚完善

--- components/Base/YzpFooter.vue
+++ components/Base/YzpFooter.vue
@@ -1,5 +1,7 @@
 <script lang="ts" setup>
 
+const { $baseSettings } = useNuxtApp()
+
 const backTopButtonShow = ref(false)
 
 const backToTop = () => {
@@ -27,7 +29,17 @@ onMounted(() => {
 
 <template>
   <footer class="yzp-footer">
-    <div class="yzp-footer-main max-w1200"></div>
+    <div class="yzp-footer-main max-w1200">
+      <div class="yzp-footer-link">
+        <nuxt-link to="/feedback">反馈建议</nuxt-link>
+        <nuxt-link to="/blogroll">友情链接</nuxt-link>
+        <nuxt-link to="/about">关于本站</nuxt-link>
+      </div>
+      <div class="yzp-footer-copyroght">
+        <span>CopyRight ©{{ new Date($baseSettings.web_date).getFullYear() + '-' + new Date().getFullYear() + $baseSettings.web_name }} - </span>
+        <a href="https://beian.miit.gov.cn/" target="_blank">{{ $baseSettings.web_beian }}</a>
+      </div>
+    </div>
     <div v-if="backTopButtonShow" class="yzp-backtop" @click="backToTop"></div>
   </footer>
 </template>
@@ -38,6 +50,7 @@ onMounted(() => {
   display: flex;
   align-items: center;
   justify-content: center;
+  padding: var(--space-15) 0;
 
   .yzp-footer-main {
     width: 100%;
@@ -55,6 +68,44 @@ onMounted(() => {
     right: 20px;
     cursor: pointer;
     z-index: 9;
+  }
+  .yzp-footer-link {
+    color: #999999;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    a {
+      display: inline-block;
+      &:hover {
+        color: #ffffff;
+        text-decoration: underline;
+      }
+      &:last-child {
+        &::after {
+          display: none;
+        }
+      }
+      &:after {
+        content: '';
+        display: inline-block;
+        height: 10px;
+        width: 1px;
+        background: #555;
+        margin: 0 15px;
+      }
+    }
+  }
+  .yzp-footer-copyroght {
+    margin-top: var(--space-15);
+    text-align: center;
+    color: #999999;
+    a {
+      display: inline-block;
+      &:hover {
+        color: #ffffff;
+        text-decoration: underline;
+      }
+    }
   }
 }
 </style>
